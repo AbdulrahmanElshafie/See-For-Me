c444e7f0d3cdf92579eb73a64f143f52c67ec7e0
Abdulrahman Elshafie <sabdo6177@gmail.com>
2023-11-07 11:17:46 +0200

    Updated API

--- Components/Components.py
+++ Components/Components.py
@@ -6,7 +6,6 @@ import cv2
 import pytesseract
 from pytesseract import Output
 
-
 def text_correction(text):
     fix_spelling = pipeline("text2text-generation", model="oliverguhr/spelling-correction-english-base")
     text = fix_spelling(text, max_length=30000)
--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from Features.Reader.Reader import Reader
 from Features.Perception.Perception import Perception
 from Features.Navigator.Navigator import Navigator
+from flask import Flask, request, jsonify, send_file
+
+app = Flask(__name__)
 
 choice = int(input('Enter your choice: '))
-if choice == 1:
-    img = ''
+
+
+@app.route("/ocr")
+def OCR():
+    img = request.files['image']
     reader = Reader()
     txt_read = reader.pipline(img)
 
-elif choice == 2:
+
+@app.route("/ocraudio")
+def getTxtRead():
+    mp3FilePath = 'mp3s/1.mp3'
+    return send_file(mp3FilePath, mimetype='audio/mpeg', as_attachment=True)
+
+
+@app.route("/navigator")
+def Navigation():
     navigator = Navigator()
 
-elif choice == 3:
+
+@app.route("/perception")
+def Perception():
     perception = Perception()
