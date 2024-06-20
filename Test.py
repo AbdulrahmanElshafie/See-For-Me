3c70c2549abef9917ccfc3f75abf7a2b9108a4cb
DELL <maryamnzlawy@gmail>
2024-06-20 22:13:57 +0300

    edit importing

--- Test.py
+++ Test.py
@@ -1,8 +1,8 @@
-#from Components.Components import *
-from Features.Perception.Perception import Perception
+from Components.Components import *
+from Components.Features import *
 
 # reader = Reader()
 # text_to_speech('hello')
-# 
+
 perceptor = Perception()
 description = perceptor.pipline("C:\\Users\\DELL\\Desktop\\pics\\2.png")
