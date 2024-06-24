e455e3c094bf909e6129e6f5972ba33aa651f092
DELL <maryamnzlawy@gmail>
2024-06-24 11:30:31 +0300

    edit the server to gunicorn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Components.Components import *
 from Features.Perception import Perception
+from gunicorn.app.base import Application
 
 app = Flask(__name__)
 
@@ -8,11 +9,22 @@ app = Flask(__name__)
 def default():
     return 'hello client'
 
+
 @app.route("/describe")
 def describe_image():
     img = request.files['image']
     perception = Perception()
     perception.pipline(img)
 
+if __name__ == '__main__': 
+   class FlaskApp(Application):
+       def load_config(self): 
+           config = super().load_config() 
+           config['bind'] = '0.0.0.0:8000'# Adjust port if needed 
+           config['workers'] = 1# Adjust workers as needed
+           return config 
 
-app.run()
+       def load_wsgi_app(self):
+           return app 
+
+FlaskApp().run()
