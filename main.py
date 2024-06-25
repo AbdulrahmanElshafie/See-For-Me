3b90813627b8b982bf3ce34593d9022631f97c0a
DELL <maryamnzlawy@gmail>
2024-06-25 15:21:51 +0300

    edit the position of flask app

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from Components.Components import *
 from Features.Perception import Perception
 from gunicorn.app.base import Application
 
-app = Flask(__name__)
+class FlaskApp(Application):
+    def load_config(self): 
+        config = super().load_config() 
+        config['bind'] = '0.0.0.0:8000'# Adjust port if needed 
+        return config 
+    def load_wsgi_app(self):
+        return app 
+    
 
+app = Flask(__name__)
 
 @app.route("/")
 def default():
     return 'hello client'
-
 
 @app.route("/describe")
 def describe_image():
@@ -16,14 +23,6 @@ def describe_image():
     perception = Perception()
     perception.pipline(img)
 
+       
 if __name__ == '__main__': 
-   class FlaskApp(Application):
-       def load_config(self): 
-           config = super().load_config() 
-           config['bind'] = '0.0.0.0:8000'# Adjust port if needed 
-           return config 
-
-       def load_wsgi_app(self):
-           return app 
-
-FlaskApp().run()
+     FlaskApp().run()
